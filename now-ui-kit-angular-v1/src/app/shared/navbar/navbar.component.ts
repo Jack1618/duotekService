e41f93ee89c59a6d84a29bdef082199a550f7aa5
Jack1618 <giacomocalcara94@gmail.com>
2018-11-23 00:02:02 +0100

    Impostazione ambiente Win, implementato scrolling

--- now-ui-kit-angular-v1/src/app/shared/navbar/navbar.component.ts
+++ now-ui-kit-angular-v1/src/app/shared/navbar/navbar.component.ts
@@ -54,4 +54,10 @@ export class NavbarComponent implements OnInit {
             return false;
         }
     }
+
+
+    scroll(elementId){
+        document.getElementById(elementId).scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+    }
+
 }
